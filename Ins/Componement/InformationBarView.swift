393ac6c995c0711fc2e819fffe625495785554fb
East <eastpak8524@gmail.com>
2020-11-11 17:55:17 +0100

    Add the icon bar and organize the files

---
+++ Ins/Componement/InformationBarView.swift
@@ -0,0 +1,35 @@
+//
+//  InformationBarView.swift
+//  Ins
+//
+//  Created by Romain Beaujean on 11/11/2020.
+//
+
+import SwiftUI
+
+struct InformationBarView: View {
+       let images = Images()
+       var body: some View {
+              HStack(alignment: .center, spacing: 15) {
+                     Image(images.icon)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 45, height: 45)
+                            .clipShape(Circle())
+                     
+                     Text("East")
+                     
+                     Spacer()
+                     Image(systemName: "ellipsis")
+                            .imageScale(.large)
+              }
+              .padding(.horizontal, 10)
+              .padding(.vertical, 10)
+       }
+}
+
+struct InformationBarView_Previews: PreviewProvider {
+       static var previews: some View {
+              InformationBarView()
+       }
+}
---
+++ Ins/Data/Images.swift
@@ -0,0 +1,15 @@
+//
+//  Images.swift
+//  Ins
+//
+//  Created by Romain Beaujean on 11/11/2020.
+//
+
+import Foundation
+import SwiftUI
+
+struct Images {
+       let burger = "burger"
+       let icon = "icon"
+       let sunset = "sunset"
+}
