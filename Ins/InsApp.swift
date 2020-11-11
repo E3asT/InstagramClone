4f35f8703f7399cd7f3c2637a0f9ad32415e6b44
East <eastpak8524@gmail.com>
2020-11-11 12:22:32 +0100

    Initial Commit

---
+++ Ins/InsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsApp.swift
+//  Ins
+//
+//  Created by Romain Beaujean on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct InsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
